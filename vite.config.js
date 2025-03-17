33e090c56249758e5f4665d1dbbffdeacaff0c1a
hakunno <samuelledrof@gmail.com>
2025-03-17 21:29:18 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/cargofran/", // ✅ If deploying in a subdirectory, set this correctly
+  base: "/cargofran/",
   server: {
-    historyApiFallback: true, // ✅ Fixes React Router 404 issue on refresh
+    historyApiFallback: true,
   },
 });
